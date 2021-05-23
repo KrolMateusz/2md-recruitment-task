c069f84aed8aaae5b0219e291a35432bca494cf9
Mateusz Król <matek8952@gmail.com>
2021-05-23 12:13:40 +0200

    Modal close button outline

--- src/components/modal/Modal.jsx
+++ src/components/modal/Modal.jsx
@@ -7,7 +7,7 @@ function Modal({ isOpened, onClose, children }) {
             <div className="border border-solid bg-blue-light h-4/5 w-4/5 md:h-3/5 md:w-3/5 lg:w-2/5 mx-auto my-auto flex flex-col items-stretch fixed inset-0 z-20">
                 <div className="h-1/5 bg-blue-light-2 text-blue-dark p-5 w-full relative flex flex-col justify-center">
                     <button
-                        className="bg-blue-light text-center w-6 h-6 rounded-full absolute right-2 top-2"
+                        className="bg-blue-light text-center w-6 h-6 rounded-full absolute right-2 top-2 focus:outline-none focus:shadow-none"
                         onClick={onClose}
                     >
                         x
